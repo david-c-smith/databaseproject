3fde09e43b8badd336aecf72f308903d4dae1830
david-c-smith <dcsmith0515@gmail.com>
2016-12-01 00:49:38 -0500

    searches display multiple entries

--- gradereport/gradereport.php
+++ gradereport/gradereport.php
@@ -4,7 +4,10 @@
   <meta charset="UTF-8">
   <title>Grade report</title>
 
+  <link href='https://fonts.googleapis.com/css?family=Lato' rel='stylesheet' type='text/css'>
 
+  <link rel='stylesheet prefetch' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.5.0/css/font-awesome.min.css'>
+  <link rel="stylesheet" href="css/style.css">
   <link rel='stylesheet prefetch' href='https://fonts.googleapis.com/css?family=Open+Sans'>
 
       <link rel="stylesheet" href="css/style.css">
@@ -13,6 +16,9 @@
 </head>
 
 <body>
+
+  <?php include_once('../navbar.html') ?>
+
   <table>
   <caption>Grade report</caption>
   <thead>
@@ -33,6 +39,12 @@
     <tr>
       <td scope="row">OOP</td>
       <td>Charles Dierbach</td>
+      <td>3</td>
+      <td>A</td>
+    </tr>
+    <tr>
+      <td scope="row">Requirements</td>
+      <td>Suranjan Chakraborty</td>
       <td>3</td>
       <td>A</td>
     </tr>
